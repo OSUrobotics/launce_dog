b90642530f2ae64243b18a04ea08d01a0db5b2f0
Benjamin Narin <narinb@onid.oregonstate.edu>
2014-08-14 14:09:02 -0700

    controller now running

--- crab_state_machine/scripts/crab_controller.py
+++ crab_state_machine/scripts/crab_controller.py
@@ -7,14 +7,23 @@ import smach_ros
 from geometry_msgs.msg import Twist
 from turtlebot_msgs.srv import SetFollowStateRequest 
 from turtlebot_msgs.srv import SetFollowStateResponse 
+from sensor_msgs.msg import Joy
+
 #define state follower
 class stop(smach.State):
 	def __init__(self):
-		smach.State.__init__(self, outcomes = ['outcome1'])
-		self.stop_pub = rospy.Publisher('cmd_vel_mux/input/teleop', twist)
+		smach.State.__init__(self, outcomes = ['outcome1','outcome2'])
+		rospy.wait_for_service('change_state')
+		while (change_state(SetFollowStateRequest.STOPPED).result != SetFollowStateResponse.OK):
+			pass
+		self.count = 0
+		self.stop_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist)
+		self.a_bool = 0
 
-	def execute(self):
-		rospy.logino('Executing State stop')
+
+
+	def execute(self, userdata):
+		rospy.loginfo('Executing State stop')
 		self.joy_sub = rospy.Subscriber("/joy",Joy,self.joy_callback)
 		self.command = Twist()
 		self.command.linear.x = 0.0
@@ -24,23 +33,29 @@ class stop(smach.State):
 		self.command.angular.y = 0.0
 		self.command.angular.z = 0.0
 		self.stop_pub.publish(self.command)
+		if (self.count == 10):
+			return 'outcome2'
+		self.count = self.count + 1
 		while (self.a_bool == 0):
-			rospy.logino('stop State Waiting')
+			wait = 1
+			rospy.loginfo('stop State Waiting')
 		self.joy_sub.unregister()
-		return outcome1
+		self.a_bool = 0 
+		return 'outcome1'
 
 	def joy_callback (self, joy_values):
-		self.a_bool = 0
-		self.buttons = joy._values.buttons
+		self.buttons = joy_values.buttons
 		if (self.buttons[0] == 1):
 			self.a_bool = 1
 
 class follower(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes = ['outcome1'])
+		self.a_bool = 0
 
-	def execute(self):
-		rospy.logino('Executing State follower')
+
+	def execute(self, userdata):
+		rospy.loginfo('Executing State follower')
 		self.joy_sub = rospy.Subscriber("/joy",Joy,self.joy_callback)
 		
 		rospy.wait_for_service('change_state')
@@ -49,45 +64,49 @@ class follower(smach.State):
 			pass
 
 		while (self.a_bool == 0):
-			rospy.logino('follower State Waiting')
+			rospy.loginfo('follower State Waiting')
 		while (change_state(SetFollowStateRequest.STOPPED).result != SetFollowStateResponse.OK):
 			pass
 		self.joy_sub.unregister()
-		return outcome1
+		self.a_bool = 0 
+		return 'outcome1'
 
 	def joy_callback (self, joy_values):
-		self.a_bool = 0
-		self.buttons = joy._values.buttons
+		self.buttons = joy_values.buttons
 		if (self.buttons[0] == 1):
 			self.a_bool = 1
 
 class wanderer(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes = ['outcome1'])
-		self.wanderer_pub = rospy.Publisher('cmd_vel_mux/input/teleop', twist)
+		self.wanderer_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist)
+		self.a_bool = 0
 
-	def execute(self):
-		rospy.logino('Executing State wanderer')
+
+	def execute(self, userdata):
+		rospy.loginfo('Executing State wanderer')
 		self.joy_sub = rospy.Subscriber("/joy",Joy,self.joy_callback)
 		#Service to Start Follower
 		while (self.a_bool == 0):
-			rospy.logino('wanderer State Waiting')
+			rospy.loginfo('wanderer State Waiting')
 		#Service to stop Follower
 		self.joy_sub.unregister()
-		#Return Outcome2
+		self.a_bool = 0 
+		return 'outcome1'
 
 	def joy_callback (self, joy_values):
-		self.a_bool = 0
-		self.buttons = joy._values.buttons
+		self.buttons = joy_values.buttons
 		if (self.buttons[0] == 1):
 			self.a_bool = 1
 
 class sit(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes = ['outcome1'])
+		self.sit_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist)
+		self.a_bool = 0
 
-	def execute(self):
-		rospy.logino('Executing State sit')
+	def execute(self, userdata):
+		rospy.loginfo('Executing State sit')
 		self.joy_sub = rospy.Subscriber("/joy",Joy,self.joy_callback)
 		self.command = Twist()
 		self.command.linear.x = 0.0
@@ -96,16 +115,15 @@ class sit(smach.State):
 		self.command.angular.x = 0.0
 		self.command.angular.y = 0.0
 		self.command.angular.z = 0.0
-		self.stop_pub.publish(self.command)
+		self.sit_pub.publish(self.command)
 		while (self.a_bool == 0):
-			rospy.logino('sit State Waiting')
-		#Service to stop Follower
+			rospy.loginfo('sit State Waiting')
 		self.joy_sub.unregister()
-		#Return Outcome4
+		self.a_bool = 0 
+		return 'outcome1'
 
 	def joy_callback (self, joy_values):
-		self.a_bool = 0
-		self.buttons = joy._values.buttons
+		self.buttons = joy_values.buttons
 		if (self.buttons[0] == 1):
 			self.a_bool = 1			
 
@@ -113,28 +131,34 @@ def main():
 	rospy.init_node('crab_controller')
 
 	#Create SMACH State MAachine
-	sm = smach.StateMachine(=['outcome2'])
+	sm = smach.StateMachine(outcomes = ['outcome3'])
 
 	#open the container
 	with sm:
 		#Add states to the container
 		smach.StateMachine.add('stop', stop(),
-								transitions={'outcome1': 'follower_1'})
-		
+								transitions = {'outcome1': 'follower_1','outcome2': 'outcome3'})
+
 		smach.StateMachine.add('follower_1', follower(),
-								transitions={'outcome1': 'wanderer'})
+								transitions = {'outcome1': 'wanderer'})
 
 		smach.StateMachine.add('wanderer', wanderer(),
-								transitions={'outcome1': 'follower_2'})
+								transitions = {'outcome1': 'follower_2'})
 
 		smach.StateMachine.add('follower_2', follower(),
-								transitions={'outcome1': 'sit'})
+								transitions = {'outcome1': 'sit'})
 
 		smach.StateMachine.add('sit', sit(),
-								transitions={'outcome1': 'stop'})
+								transitions = {'outcome1': 'stop'})
 
+		
 		#Execute SMACH plan
-		outcome = sm.execute()
+	sis = smach_ros.IntrospectionServer('virtual_smach', sm, '/SM_ROOT')
+	sis.start()
+	outcome = sm.execute()
+
+	rospy.spin()
+	sis.stop()
 
 if __name__ == '__main__':
 	main()
